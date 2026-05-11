e2a579dd3e1ac413e9baf492eca50e626e71ac49
jalzae <jalzae@gmail.com>
2026-05-11 12:43:21 +0700

    chore: remove redundant 60s timeout configuration in cucumber.js

--- cucumber.js
+++ cucumber.js
@@ -8,7 +8,6 @@ module.exports = {
     "--require src/steps.ts",
     "--require src/hooks.ts",
     "--format json:cucumber-report.json",
-    "--timeout 60000",
   ].join(" "),
   afterLaunch: async function () {
     const htmlReportOptions = {
